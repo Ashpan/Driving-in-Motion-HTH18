c331cdce53cb079463b4ee8bc06a7bdba6e5ab06
D-Harsh <harsh200014@gmail.com>
2018-02-04 10:11:30 -0500

    minor changes

--- src/main/FileWriterFU.java
+++ src/main/FileWriterFU.java
@@ -10,7 +10,7 @@ public class FileWriterFU {
       throws IOException {
     	a /= 90;
 		System.out.println(System.getProperty("user.dir"));
-		BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"\\src\\main\\stream.txt", true));
+		BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Harsh\\Desktop\\race_Data\\Resources\\stream.txt", true));
 		if(!Double.toString(a).equals("NaN")) {
 			writer.append(Double.toString(a));
 			writer.newLine();
@@ -18,7 +18,7 @@ public class FileWriterFU {
         writer.close();
     }
     public static void delete() throws IOException{
-    	PrintWriter writer = new PrintWriter("C:\\Users\\Harsh\\Desktop\\HTH18\\src\\main\\stream.txt");
+    	PrintWriter writer = new PrintWriter("C:\\Users\\Harsh\\Desktop\\race_Data\\Resources\\stream.txt");
     	writer.print("");
     	writer.close();
     }
